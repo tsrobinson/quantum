2b9c08a6493476b8c889084d00b9f0716b906b03
Tom Robinson <39620643+tsrobinson@users.noreply.github.com>
2022-07-10 12:54:10 +0100

    add note

--- quantum.py
+++ quantum.py
@@ -24,6 +24,8 @@ job2 = simulator.run(qc2)
 result2 = job2.result()
 result2.get_counts()
 
+## Note seems that [2] is the third order, so for [0,1,2] the encoded string is f"[2][1][0]"
+
 # Test: encoding the number four to binary (4 -> 100)
 
 qc3 = QuantumCircuit(3,3)
